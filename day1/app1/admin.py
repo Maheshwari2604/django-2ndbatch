62f292c4bd3547d7b646d521944b90f50261a221
Maheshwari2604 <maheshwarishivam2604@gmail.com>
2019-07-10 16:06:23 +0530

    2nd day

--- day1/app1/admin.py
+++ day1/app1/admin.py
@@ -1,8 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import homee
+from .models import homee , address
 from django.contrib import admin
 
 # Register your models here.
+class homeee(admin.ModelAdmin):
+    list_display = ['firstname' , 'lastname' , 'email']
+    list_editable = ['email']
+    search_fields = ['email']
+    date_hierarchy = 'timestamp'
+    readonly_fields = ['timestamp', 'updated']
 
-admin.site.register(homee)
+    class meta:
+        model = homee
+
+admin.site.register(homee, homeee)
+
+
+admin.site.register(address)
+
+
+
+#KNCBDAJCVAG.COM/shivam-maheshwari
+    
--- day1/app1/models.py
+++ day1/app1/models.py
@@ -5,10 +5,10 @@ from django.db import models
 
 # Create your models here.
 class homee(models.Model):
-    firstname = models.CharField(max_length=250, help_text='Required')
-    lastname = models.CharField(max_length=250, help_text='Required')
-    username = models.CharField(max_length=250, help_text='Required')
-    email = models.EmailField(max_length=250, help_text='Required')
+    firstname = models.CharField(max_length=250)
+    lastname = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
     #contact_no = models.IntegerField(null=True)
     #slug = models.SlugField(unique=True, null=True)
     password = models.CharField(max_length=100)
@@ -17,3 +17,17 @@ class homee(models.Model):
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     email_verified = models.BooleanField(default=False)
     is_active = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return str(self.id)
+
+
+class address(models.Model):
+    city = models.CharField(max_length=30)
+    service_area = models.CharField(max_length=100)
+    local_address = models.CharField(max_length=200)
+    landmark = models.CharField(max_length=120, null=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __unicode__(self):
+        return str(self.user)
